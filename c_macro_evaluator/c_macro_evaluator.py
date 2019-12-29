7d625953154515e1937346e576dbe0573ea30f21
Sturla Lange <sturla22@gmail.com>
2019-12-29 13:09:21 +0000

    initial commit

---
+++ c_macro_evaluator/c_macro_evaluator.py
@@ -0,0 +1,106 @@
+import re
+from typing import Dict, List
+
+
+class MacroEvaluator:
+    """."""
+
+    MACRO_REGEX = re.compile(r"^\s*#\s*(\w+)\s*(\w*)\s*([^\\|^\n]*)(\\*)$")
+    MULTILINE_REGEX = re.compile(r"^\s*([^\\|^\n]*)(\\*)$")
+    LITERAL_SUFFIX_REGEX = re.compile(r"((?:0[xXoO])?\d*)([uUlL]+)")
+    INCLUDE_REGEX = re.compile(r"[\"<](.*)[\">]")
+
+    def __init__(self, lines: List[str] = [], file_name: str = ""):
+        if lines:
+            self.lines = lines
+        elif file_name:
+            self.lines = self.read_lines(file_name)
+        self.symbols: Dict[str, str] = {}
+        self.inactive_level = 0
+        self.parse_lines()
+
+    def read_lines(self, file_name) -> List[str]:
+        lines: List[str]
+        with open(file_name, "r") as f:
+            lines = f.readlines()
+        return lines
+
+    def handle_multiline(self, line, parsed_lines) -> bool:
+        m = self.MULTILINE_REGEX.match(line)
+        if m:
+            line_groups = m.groups()
+            # Append to the line that was just parsed.
+            parsed_lines[-1][-2] += line_groups[0].strip()
+            return bool(line_groups[1])
+        return True
+
+    def handle_directives(self, line):
+        command = line[0].lower()
+        if line[1]:
+            symbol = line[1]
+            if command == "ifdef":
+                if symbol not in self.symbols or self.inactive_level:
+                    self.inactive_level += 1
+            elif command == "ifndef" or self.inactive_level:
+                if symbol in self.symbols:
+                    self.inactive_level += 1
+            elif command == "if" or self.inactive_level:
+                if not self.evaluate_macro(symbol):
+                    self.inactive_level += 1
+            elif command == "pragma" and not self.inactive_level:
+                if line[2]:
+                    print(line[2])
+            elif command == "undef" and not self.inactive_level:
+                del self.symbols[symbol]
+            elif command == "define" and not self.inactive_level:
+                if line[2]:
+                    self.symbols[symbol] = line[2]
+                else:
+                    self.symbols[symbol] = None
+        else:
+            if command == "endif" and self.inactive_level:
+                self.inactive_level -= 1
+            elif command == "error" and not self.inactive_level:
+                if line[2]:
+                    raise UserWarning(line[2])
+                else:
+                    raise UserWarning("Error directive hit")
+            elif command == "include" and not self.inactive_level:
+                # TODO(stla): Detect circular dependency.
+                if line[2]:
+                    file_name = self.INCLUDE_REGEX.sub(r"\1", line[2])
+                    e = MacroEvaluator(file_name=file_name)
+                    self.symbols.update(e.symbols)
+                else:
+                    raise UserWarning("Include without filename")
+
+    def parse_lines(self) -> List[List[str]]:
+        parsed_lines: List[List[str]] = []
+        multiline = False
+        # TODO(stla): Detect unmatched if statements.
+        self.inactive_level = 0
+        for line in self.lines:
+            if multiline:
+                multiline = self.handle_multiline(line, parsed_lines)
+            m = self.MACRO_REGEX.match(line)
+            if m:
+                line_groups = list(m.groups())
+                if len(line_groups) > 3 and line_groups[3]:
+                    multiline = True
+                parsed_lines.append(line_groups)
+        for parsed_line in parsed_lines:
+            self.handle_directives(parsed_line)
+
+        return parsed_lines
+
+    def evaluate_macro(self, macro_name):
+        """Evaluate the parsed macros."""
+        # TODO(stla): Handle ctypes and casts.
+        macro = self.symbols[macro_name]
+        for symbol in self.symbols.keys():
+            # TODO(stla): Detect circular dependency.
+            if symbol in macro:
+                result = self.evaluate_macro(symbol)
+                macro = macro.replace(symbol, str(result))
+            macro = self.LITERAL_SUFFIX_REGEX.sub(r"\1", macro)
+        return eval(macro)
---
+++ test/test_c_macro_evaluator.py
@@ -0,0 +1,74 @@
+from c_macro_evaluator import c_macro_evaluator as dut
+
+macro_str = "#define TEST"
+not_macro_str = "define TEST2"
+multiline_macro = r"""#define MACRO 1+2\
+        +4+5\
+        *3
+"""
+recursive_macro = "#define RECURSIVE MACRO+1"
+circular_macro = "#define CIRCULAR CIRCULAR*2"
+conditional_macro = r"""
+#define COND1
+#ifdef COND1
+#define COND2 0O1
+#if COND2
+#define COND3 0x0U
+#if COND3
+#define COND4 "Should not get here!"
+#error
+#endif // COND3
+#endif // COND2
+#endif // COND1
+#undef COND1
+#ifdef COND1
+#define COND4 "Not here either!"
+#error
+#endif
+"""
+
+
+def test_macro_regex() -> None:
+    assert dut.MacroEvaluator.MACRO_REGEX.match(macro_str)
+    assert not dut.MacroEvaluator.MACRO_REGEX.match(not_macro_str)
+
+
+def test_symbol_finding() -> None:
+    e = dut.MacroEvaluator([macro_str, not_macro_str, *multiline_macro.split("\n")])
+    assert "TEST" in e.symbols.keys()
+    assert "TEST2" not in e.symbols.keys()
+    assert "MACRO" in e.symbols.keys()
+    assert "1+2+4+5*3" in e.symbols["MACRO"]
+    assert len(e.symbols.keys()) == 2
+
+
+def test_evaluate() -> None:
+    e = dut.MacroEvaluator(
+        [*multiline_macro.split("\n"), recursive_macro, circular_macro]
+    )
+    assert e.evaluate_macro("MACRO") == 22
+    assert e.evaluate_macro("RECURSIVE") == 23
+
+
+def test_conditional() -> None:
+    e = dut.MacroEvaluator(conditional_macro.split("\n"))
+    # COND1 gets removed by undef
+    assert "COND1" not in e.symbols.keys()
+    assert "COND2" in e.symbols.keys()
+    assert "COND3" in e.symbols.keys()
+    assert "COND4" not in e.symbols.keys()
+
+
+def test_file() -> None:
+    e = dut.MacroEvaluator(file_name="test/test_c_macro_evaluator.py")
+    assert "COND2" in e.symbols.keys()
+
+
+def test_include() -> None:
+    e = dut.MacroEvaluator(
+        r"""#include "test/test_c_macro_evaluator.py"
+    """.split(
+            "\n"
+        )
+    )
+    assert "COND2" in e.symbols.keys()
